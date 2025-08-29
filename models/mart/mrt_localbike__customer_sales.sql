8915f9e1fb810cadd7040523a1deb39bf354465d
Lola Cadène Martinache <lola@jeancadene.net>
2025-08-29 15:18:45 +0000

    feat: add a mart for sales by customer

---
+++ models/mart/mrt_localbike__customer_sales.sql
@@ -0,0 +1,8 @@
+SELECT
+    customer_id,
+    COUNT(DISSTINCT order_id) AS total_orders,
+    SUM(product_quantity) AS total_units_sold,
+    SUM(total_order_item_amount) AS total_spent,
+    AVG(total_order_item_amount) AS avg_order_item_amount
+FROM {{ ref('int_localbike_sales_fact') }}
+GROUP BY customer_id
