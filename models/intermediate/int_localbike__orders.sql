0b0f84780a35434775a657627b2b4976439e8861
Lola Cadène Martinache <lola@jeancadene.net>
2025-08-29 13:21:00 +0000

    feat: Add intermediate model for orders and stores info

---
+++ models/intermediate/int_localbike__orders.sql
@@ -0,0 +1,9 @@
+SELECT
+    o.order_id,
+    o.customer_id,
+    o.store_id,
+    s.store_name,
+    s.store_city,
+    o.order_date
+FROM {{ref('stg_localbike__orders')}} AS o
+LEFT JOIN {{ref('stg_localbike__stores')}} AS s ON o.store_id = s.store_id
