52be26c5e62628be51f5e747e75cc15aec42dcaa
Lola Cadène Martinache <lola@jeancadene.net>
2025-08-29 13:33:39 +0000

    fix: Modify column name

--- models/intermediate/int_localbike__order_items.sql
+++ models/intermediate/int_localbike__order_items.sql
@@ -2,11 +2,11 @@ SELECT
     oi.order_item_id,
     oi.order_id,
     oi.product_id,
-    p.product_name,
     c.category_id,
+    p.product_name,
     c.category_name,
     p.product_price,
-    oi.quantity,
+    oi.product_quantity,
     oi.total_order_item_amount
 FROM {{ ref('stg_localbike__order_items') }} oi
 LEFT JOIN {{ ref('stg_localbike__products') }} p on oi.product_id = p.product_id
