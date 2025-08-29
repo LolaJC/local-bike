1bf3e215aa4a6063ab9da269aa38ec250609672c
Lola Cadène Martinache <lola@jeancadene.net>
2025-08-29 13:29:43 +0000

    feat: Create intermediate model with order item, product and category info

--- models/intermediate/int_localbike__orders.sql
+++ models/intermediate/int_localbike__orders.sql
@@ -5,5 +5,5 @@ SELECT
     s.store_name,
     s.store_city,
     o.order_date
-FROM {{ref('stg_localbike__orders')}} AS o
-LEFT JOIN {{ref('stg_localbike__stores')}} AS s ON o.store_id = s.store_id
+FROM {{ ref('stg_localbike__orders') }} AS o
+LEFT JOIN {{ ref('stg_localbike__stores') }} AS s ON o.store_id = s.store_id
