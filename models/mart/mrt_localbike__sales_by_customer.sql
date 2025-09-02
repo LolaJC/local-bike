d41731647e0b9e5ce66511d1ba29fb3ffb69c274
Lola Cadène Martinache <lola@jeancadene.net>
2025-09-02 08:03:38 +0000

    fix: Remove typo

--- models/mart/mrt_localbike__sales_by_customer.sql
+++ models/mart/mrt_localbike__sales_by_customer.sql
@@ -5,6 +5,6 @@ SELECT
     SUM(product_quantity) AS total_units_sold,
     SUM(total_order_item_amount) AS total_spent,
     AVG(total_order_item_amount) AS avg_order_item_amount,
-    AVG(COUNT(DISTINCT order_id) AS avg_orders
+    AVG(COUNT(DISTINCT order_id)) AS avg_orders
 FROM {{ ref('int_localbike_sales_fact') }}
 GROUP BY customer_id
