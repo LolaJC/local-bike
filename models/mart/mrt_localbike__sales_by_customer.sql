e55460f001eb35a2cf6310444c5cfca43bff5d16
Lola Cadène Martinache <lola@jeancadene.net>
2025-09-02 08:26:57 +0000

    feat: Modify metrics for customers

--- models/mart/mrt_localbike__sales_by_customer.sql
+++ models/mart/mrt_localbike__sales_by_customer.sql
@@ -1,10 +1,8 @@
 SELECT
     customer_id,
-    COUNT(customer_id) AS nb_customers,
     COUNT(DISTINCT order_id) AS total_orders,
     SUM(product_quantity) AS total_units_sold,
     SUM(total_order_item_amount) AS total_spent,
-    AVG(total_order_item_amount) AS avg_order_item_amount,
-    AVG(COUNT(DISTINCT order_id)) AS avg_orders
+    AVG(total_order_item_amount) AS avg_order_item_amount
 FROM {{ ref('int_localbike_sales_fact') }}
 GROUP BY customer_id
