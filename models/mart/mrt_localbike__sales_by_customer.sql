05d0e2c94e2f90b2ac212b396f41c524203d819c
Lola Cadène Martinache <lola@jeancadene.net>
2025-09-02 07:31:26 +0000

    add: Add metrics in mart about consumers

--- models/mart/mrt_localbike__sales_by_customer.sql
+++ models/mart/mrt_localbike__sales_by_customer.sql
@@ -1,8 +1,10 @@
 SELECT
     customer_id,
+    COUNT(customer_id) AS nb_customers,
     COUNT(DISTINCT order_id) AS total_orders,
     SUM(product_quantity) AS total_units_sold,
     SUM(total_order_item_amount) AS total_spent,
-    AVG(total_order_item_amount) AS avg_order_item_amount
+    AVG(total_order_item_amount) AS avg_order_item_amount,
+    AVG(COUNT DISTINCT order_id) AS avg_orders
 FROM {{ ref('int_localbike_sales_fact') }}
 GROUP BY customer_id
